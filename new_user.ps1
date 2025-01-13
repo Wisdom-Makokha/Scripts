87fa474f6498db5cfab94e7f1f210f694dca2a04
Wisdom-Makokha <wisdomjmak@gmail.com>
2025-01-13 20:08:38 +0300

    first commit

--- new_user.ps1
+++
@@ -1,5 +0,0 @@
-$password = Read-Host -AsSecureString "Enter your password: "
-
-$username = Read-Host "Enter your username: "
-$fullName = Read-Host "Enter your name: "
-New-LocalUser -Name $username -Password $password -FullName $fullName -Description "standard user account"
